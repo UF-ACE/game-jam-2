d4d42931ca0455107ccec021c4ebc20b2fb0d3c9
ConnorJC <ccatlett2000@mctherealm.net>
2020-02-17 23:05:45 -0500

    Code

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+  public float reboundSpeed;
+  public float terrainSpeed;
+
+  bool grounded = false;
+
+  bool WalkSanity() {
+    if(GetComponent<Rigidbody2D>().gravityScale > 0) {
+      return transform.position.y < 0;
+    } else {
+      return transform.position.y > 0;
+    }
+  }
+
+  void Update() {
+    if(Physics2D.BoxCast(GetComponent<Transform>().position, new Vector2(5.12f * 0.8f, 5.12f * 0.8f), 0.0f, Vector2.right, 2.6f * 0.8f).collider != null) {
+      transform.Translate(Vector2.left * terrainSpeed * Time.deltaTime);
+    }
+
+    if(Input.GetKeyDown(KeyCode.Space)) {
+      GetComponent<Rigidbody2D>().gravityScale *= -1;
+      GetComponent<SpriteRenderer>().flipY = !GetComponent<SpriteRenderer>().flipY;
+    }
+    GetComponent<Animator>().SetBool("isWalking", grounded && WalkSanity());
+    
+    if(transform.position.x < -0.001f) {
+      float amountToMove = reboundSpeed * Time.deltaTime;
+
+      if(Physics2D.BoxCast(GetComponent<Transform>().position, new Vector2(5.12f * 0.8f, 5.12f * 0.8f), 0.0f, Vector2.right, 5.12f * 0.8f).collider == null) {
+        transform.Translate(amountToMove, 0, 0);
+        Debug.Log("test");
+      }
+    }
+  }
+
+  void OnCollisionStay2D(Collision2D c) {
+    if(Physics2D.Raycast(GetComponent<Transform>().position, Vector2.down, 2.57f * 0.8f).collider != null) {
+      grounded = true;
+    }
+  }
+
+  void OnCollisionExit2D(Collision2D c) {
+    grounded = false;
+  }
+}
---
+++ Assets/Scripts/TerrainController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour {
+  public GameObject[] terrain;
+  public float speed;
+
+  List<GameObject> current = new List<GameObject>();
+  void Start() {
+    for(int i = 0; i < 10; i++) {
+      NewTerrain();
+    }
+  }
+
+  void Update() {
+    foreach(GameObject g in current) {
+      g.transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+    if(current[0].transform.position.x < 12 * -5 * 5.12f) {
+      NewTerrain();
+    }
+  }
+
+  void NewTerrain() {
+    if(current.Count >= 10) {
+      GameObject.Destroy(current[0]);
+      current.RemoveAt(0);
+    }
+    if(current.Count > 0) {
+      GameObject g = terrain[Random.Range(0, terrain.Length)];
+      current.Add(GameObject.Instantiate(g, current[current.Count - 1].transform.position + new Vector3(12 * 5.12f, 0, 0), Quaternion.identity, GetComponent<Transform>()));
+    } else {
+      current.Add(GameObject.Instantiate(terrain[0], GetComponent<Transform>()));
+    } 
+  }
+}
