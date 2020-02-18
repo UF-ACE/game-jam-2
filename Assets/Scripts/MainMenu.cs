31bb45c6ce7d6324df0caf2d440ba00fa61160ec
Christopher Miller <admin@mctherealm.net>
2020-02-17 21:10:46 -0500

    Added MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPressExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnPressStart() {
+        SceneManager.LoadScene("Game");
+    }
+}
